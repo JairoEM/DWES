19580bd9a29598cf005f85076e852169ae19c144
JairoEM <loseloth@gmail.com>
2018-10-30 09:20:06 +0100

    From works, carrito by Victor

--- Unit 7/Form/ejercicio1-resultados.php
+++ Unit 7/Form/ejercicio1-resultados.php
@@ -6,17 +6,24 @@
     $color = $_POST["selectSimple"];
     $textArea = $_POST["textArea"];
 
+    echo("<h1>User Info</h1>");
     echo("The text into the text field was: $text");
+    echo("<br>");
     echo("You chose the option $radio as your gender.");
+    echo("<br>");
     echo("The name of the field that you upload was $file.");
+    echo("<br>");
     echo("Your password is $password");
+    echo("<br>");
     echo("You choses $color as your favourite color.");
+    echo("<br>");
     echo("The text into the text area was : $textArea");
+    echo("<br>");
 
     $checkbox = $_POST["extra"];
-    $languate = $_POST["selectMultiple"];
+    $language = $_POST["selectMultiple"];
 
-    if(empty($checkbox)) {
+    if(empty($checkbox)){
         echo("You didn't select any extra.");
     }else{
         $N = count($checkbox);
@@ -25,14 +32,16 @@
             echo($checkbox[$i] . " ");
         }
     }
+    echo("<br>");
 
-    if(empty($checkbox)) {
+    if(empty($language)){
         echo("You didn't select any language.");
     }else{
-        $J = count($checkbox);
-        echo("You selected $N language(s): ");
-        for($i=0; $i < $J; $i++){
-            echo($checkbox[$i] . " ");
+        $J = count($language);
+        echo("You selected $J language(s): ");
+        for($k=0; $k < $J; $k++){
+            echo($language[$k] . " ");
         }
     }
+    echo("<br>");
 ?>
